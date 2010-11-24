5f5580fb73ce9906b32a438575c0cfc953600c15
Gary Strong <gary.m.strong@gmail.com>
2010-11-24 00:34:37 +0000

    initial commit

---
+++ Spe.sh.Net/Controllers/ErrorController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Spe.sh.Net.Controllers
+{
+    public class ErrorController : Controller
+    {
+        //
+        // GET: /Error/NotFound
+        public ActionResult NotFound(FormCollection collection)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Error/Problem
+        public ActionResult Problem(FormCollection collection)
+        {
+            return View();
+        }
+    }
+}
---
+++ Spe.sh.Net/Data/data.sql
@@ -0,0 +1,1 @@
+﻿INSERT INTO short_url VALUES ('testhw', 'aHR0cDovL3d3dy5tcmNldHJhYy5ubA==', DATETIME('NOW'), DATETIME('NOW'));
